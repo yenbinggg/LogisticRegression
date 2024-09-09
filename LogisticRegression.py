4fb509d6c6838572537abc87f04a1852b6625a47
yenbinggg <lokyb-wm22@student.tarc.edu.my>
2024-09-09 19:48:08 +0800

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,6 +2,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import streamlit as st
 import nltk
+from joblib import test
 
 data = pd.read_csv(r'C:\Users\yenbing\OneDrive\Documents\AI Assignment\diabetes_prediction_dataset.csv')
 
@@ -9,5 +10,7 @@ data = pd.read_csv(r'C:\Users\yenbing\OneDrive\Documents\AI Assignment\diabetes_
 from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression()
 lr.fit(X_train, y_train)
+
+
 
 y_pred_lr = lr.predict(X_test)
